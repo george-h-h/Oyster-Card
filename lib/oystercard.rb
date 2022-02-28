859420d5d717a39d1ff9ec891a1fb7355caab2f8
George Hett <georgehett95@gmail.com>
2022-02-28 17:19:57 +0000

    added minimum balance tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,40 @@
 class Oystercard
 
+  attr_reader :balance
+  TOP_UP_LIMIT = 90
+
+  def initialize(max_limit = TOP_UP_LIMIT)
+    @max_limit = max_limit
+    @balance = 0
+    @in_journey = false
+  end
+
+  def top_up(money)
+    if @balance + money > @max_limit
+      raise Exception.new "Max balance of £#{TOP_UP_LIMIT} reached"
+    else 
+      return @balance += money
+    end
+  end
+
+  def deduct(money)
+    return @balance -= money
+  end
+
+  def touch_in
+    if @balance < 1
+      raise Exception.new "Must have at least minimum balance to touch in"
+    else
+      @in_journey = true
+    end
+  end
+
+  def in_journey?
+    return @in_journey
+  end
+
+
+  def touch_out
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,43 @@ require 'oystercard'
 
 describe Oystercard do
 
+  context "deals with balance, topping up and max limit" do
+    it "starts with a balance of 0" do
+      expect(subject.balance).to eq(0)
+    end
+
+    it "can be topped up" do
+      expect(subject.top_up(3)).to eq(3)
+    end
+
+    it "top up raises exception if new balance would exceed limit" do
+      expect {subject.top_up(91)}.to raise_error("Max balance of £90 reached")
+    end
+
+    it "deducts money" do
+      subject.top_up(50)
+      expect(subject.deduct(10)).to eq(40)
+    end
+  end
+
+  it "supports touch in" do
+    subject.top_up(25)
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it "is initially not in a journey" do
+    expect(subject).not_to be_in_journey
+  end
+
+  it "supports touch out" do
+    subject.top_up(25)
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+
+  it "touch in raises exception if balance is less than £1" do
+    expect {subject.touch_in}.to raise_error("Must have at least minimum balance to touch in")
+  end
 end
